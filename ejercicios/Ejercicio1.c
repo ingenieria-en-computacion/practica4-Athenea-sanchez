2233af5b50b83c058f2cf1bf71f8f0ae714ce265
Athenea Sánchez Torres <sancheztorresathenea@gmail.com>
2025-03-06 19:00:14 -0600

    Add files via upload

---
+++ ejercicios/Ejercicio1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n; 
+    printf("Ingrese el tamaño del arreglo: ");
+    scanf("%d",&n); 
+    
+    int *arre=NULL; 
+    arre=(int*)malloc(n*sizeof(int)); 
+
+    if (arre == NULL) {
+        printf("Error: No se pudo asignar memoria.\n");
+        return 1;
+    }
+
+    printf("Ingrese %d valores:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d",*(arre+i));  
+    }
+
+    printf("El arreglo es: ");
+    for (int i = 0; i < n; i++) {
+        printf("\t %d", *(arre+i)); 
+        
+    }
+    printf("\n");
+
+    
+    return 0;
+}
